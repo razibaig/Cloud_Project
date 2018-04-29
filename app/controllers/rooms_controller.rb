1c0694f9383e61f192f4e55d8088444ff0500135
razibaig <mrrazi.baig@gmail.com>
2018-04-29 17:44:55 +0500

    few issues fix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -30,7 +30,7 @@ class RoomsController < ApplicationController
     @room.is_booked = false
     respond_to do |format|
       if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
+        format.html { redirect_to rooms_path}
         format.json { render :show, status: :created, location: @room }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class RoomsController < ApplicationController
   def update
     respond_to do |format|
       if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
+        format.html { redirect_to rooms_path}
         format.json { render :show, status: :ok, location: @room }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class RoomsController < ApplicationController
   def destroy
     @room.destroy
     respond_to do |format|
-      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
+      format.html { redirect_to rooms_path }
       format.json { head :no_content }
     end
   end
